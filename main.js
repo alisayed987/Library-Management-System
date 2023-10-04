a3b573fda54d8391a1a242a3ea64a1bdb3946c0f
ali.sayed987 <ali.sayed987@yahoo.com>
2023-10-05 00:11:07 +0300

    add enironment vars and configs for DB and establish connection

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+const sequelize = require('./database/db');
+
+port = process.env.APP_PORT;
+const server = app.listen(port, () => {
+    console.log(`Furniture store app listening at http://localhost:${port}`);
+});
+
+module.exports = server;
