95f4e002908ff7f9f133e43731bc324da23551e7
ali.sayed987 <ali.sayed987@yahoo.com>
2023-10-07 01:19:48 +0300

    add borrower current borrowed books API & APIs docs

--- routes/borrowers.js
+++ routes/borrowers.js
@@ -1,19 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
+const { Op } = require("sequelize");
+
 module.exports = (sequelize) => {
   const Borrower = sequelize.models.Borrower;
+  const BookBorrower = sequelize.models.BookBorrower;
 
+  /**
+   * GET all borowers
+   */
   router.get('/', async (req, res) => {
     const borrowers = await Borrower.findAll();
     res.send(borrowers);
   });
 
+  /**
+   * GET a borrower by id
+   */
   router.get('/:id', async (req, res) => {
     const borrower = await Borrower.findByPk(req.params.id);
     res.send(borrower);
   });
 
+  /**
+   * GET a borrower's current borrowed books
+   */
+  router.get('/:id/borrowedBooks', async (req, res) => {
+    const allBorrowerBooks = await BookBorrower.findAll({
+      include: [
+        {
+          model: sequelize.models.Book,
+        },
+        {
+          model: sequelize.models.Borrower,
+        }
+      ],
+      where: {
+        BorrowerId: req.params.id,
+        returnedAt: { [Op.is]: null }
+      }
+    });
+    res.send(allBorrowerBooks);
+  });
+
+  /**
+   * CREATE borrower if does not exist (unique email)
+   */
   router.post('/', async (req, res) => {
     try {
       /**
@@ -38,6 +71,9 @@ module.exports = (sequelize) => {
     }
   });
 
+  /**
+   * UPDATE a borrower by id
+   */
   router.put('/:id', async (req, res) => {
     try {
       const updated = await Borrower.update(
@@ -53,6 +89,9 @@ module.exports = (sequelize) => {
     }
   });
 
+  /**
+   * DELETE a borrower by id
+   */
   router.delete('/:id', async (req, res) => {
     try {
       const deleted = await Borrower.destroy({
